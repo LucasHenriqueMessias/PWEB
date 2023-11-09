212a8f75a6687cfeab814a8d98b0adfaa4f42544
Lucas Henrique Messias Gonçalves <64761016+LucasHenriqueMessias@users.noreply.github.com>
2023-11-08 22:09:59 -0300

    Add files via upload

---
+++ Atividade17/PWebNode/callback1.js
@@ -0,0 +1,12 @@
+// usar o npm install prompt-sync
+const prompt = require('prompt-sync')();
+
+function saudacao(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+function entradaNome(callback) {
+    var nome = prompt('Digite seu nome:');
+    callback(nome);
+}
+
+entradaNome(saudacao);
